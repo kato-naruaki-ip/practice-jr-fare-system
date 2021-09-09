3acf806bbc56683c7b6e730a7558f5efb7f2f585
Kato Naruaki <kato_naruaki@interprism.co.jp>
2021-09-09 11:39:57 +0900

    refactor(BasicFare): set @Value annotation

--- src/main/java/domain/jr/faresystem/model/basicfare/BasicFare.java
+++ src/main/java/domain/jr/faresystem/model/basicfare/BasicFare.java
@@ -2,23 +2,17 @@ package domain.jr.faresystem.model.basicfare;
 
 import domain.jr.externalsystems.station.Station;
 import domain.jr.faresystem.model.fare.Fare;
-import lombok.*;
-import lombok.experimental.FieldDefaults;
+import lombok.NonNull;
+import lombok.Value;
 
-@ToString
-@EqualsAndHashCode
-@AllArgsConstructor
-@FieldDefaults(makeFinal = true, level = AccessLevel.PRIVATE)
-public final class BasicFare {
+@Value
+public class BasicFare {
     @NonNull
-    @Getter
     Station departure;
 
     @NonNull
-    @Getter
     Station destination;
 
     @NonNull
-    @Getter
     Fare fare;
 }
