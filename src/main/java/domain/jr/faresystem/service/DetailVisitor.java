b2f6c54eca0186c641270128c961073d3a53dd28
Kato Naruaki <kato_naruaki@interprism.co.jp>
2021-09-09 15:50:58 +0900

    fix(DetailVisitor): remove fare field

--- src/main/java/domain/jr/faresystem/service/DetailVisitor.java
+++ src/main/java/domain/jr/faresystem/service/DetailVisitor.java
@@ -1,7 +1,5 @@
 package domain.jr.faresystem.service;
 
-import domain.jr.faresystem.model.fare.Fare;
-import domain.jr.faresystem.service.discount.ChildDiscount;
 import io.vavr.Tuple;
 import io.vavr.Tuple2;
 import lombok.AccessLevel;
@@ -12,20 +10,15 @@ import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@AllArgsConstructor(access = AccessLevel.PACKAGE)
 class DetailVisitor implements FareTree.FareTreeVisitor {
     private static final String HEADER_UNIT = "\t";
 
-    private Fare fare;
     private int depth;
     private List<Tuple2<Integer, String>> lines;
 
     public static DetailVisitor zero() {
-        return new DetailVisitor(Fare.from(0), 0, List.of());
-    }
-
-    public Fare getFare() {
-        return fare;
+        return new DetailVisitor(0, List.of());
     }
 
     public String show() {
@@ -36,19 +29,16 @@ class DetailVisitor implements FareTree.FareTreeVisitor {
 
     @Override
     public void visitFareLeaf(FareTree.FareLeaf node) {
-        fare = node.fare;
         lines = List.of(Tuple.of(depth, "fare: " + node.fare.show()));
     }
 
     @Override
     public void visitBasicFareLeaf(FareTree.BasicFareLeaf node) {
-        fare = node.basicFare.getFare();
         lines = List.of(Tuple.of(depth, "BasicFare: " + node.basicFare.show()));
     }
 
     @Override
     public void visitSuperExpressSurchargeLeaf(FareTree.SuperExpressSurchargeLeaf node) {
-        fare = node.superExpressSurcharge.getFare();
         lines = List.of(Tuple.of(depth, "SuperExpressSurcharge: " + node.superExpressSurcharge.show()));
     }
 
@@ -61,21 +51,17 @@ class DetailVisitor implements FareTree.FareTreeVisitor {
 
         node.subTree1.accept(visitor1);
         node.subTree2.accept(visitor2);
-        var fare1 = visitor1.fare;
-        var fare2 = visitor2.fare;
         var lines1 = visitor1.lines;
         var lines2 = visitor2.lines;
 
-        fare = fare1._plus_(fare2);
         lines = sum(
-                List.of(Tuple.of(depth, "plus: " + fare.show())),
+                List.of(Tuple.of(depth, "plus: " + EvalVisitor.evaluate(node).show())),
                 sum(lines1, lines2)
         );
     }
 
     @Override
     public void visitSumNode(FareTree.SumNode node) {
-        List<Fare> results = new ArrayList<>();
         List<List<Tuple2<Integer, String>>> lss = new ArrayList<>();
 
         for (FareTree ft : node.subTrees) {
@@ -83,14 +69,11 @@ class DetailVisitor implements FareTree.FareTreeVisitor {
             visitor.depth = depth + 1;
 
             ft.accept(visitor);
-            results.add(visitor.fare);
             lss.add(visitor.lines);
         }
 
-        fare = results.stream()
-                .reduce(Fare.from(0), Fare::_plus_);
         lines = sum(
-                List.of(Tuple.of(depth, String.format("sum(%d): %s", lss.size(), fare.show()))),
+                List.of(Tuple.of(depth, String.format("sum(%d): %s", lss.size(), EvalVisitor.evaluate(node).show()))),
                 lss.stream().reduce(List.of(), DetailVisitor::sum)
         );
     }
@@ -102,9 +85,8 @@ class DetailVisitor implements FareTree.FareTreeVisitor {
 
         node.subTree.accept(visitor);
 
-        fare = node.discount.apply(visitor.fare);
         lines = sum(
-                List.of(Tuple.of(depth, String.format("Discount(%s): %s", node.discount.getClass().getSimpleName(), fare.show()))),
+                List.of(Tuple.of(depth, String.format("Discount(%s): %s", node.discount.getClass().getSimpleName(), EvalVisitor.evaluate(node).show()))),
                 visitor.lines
         );
     }
@@ -118,19 +100,18 @@ class DetailVisitor implements FareTree.FareTreeVisitor {
 
         node.basicFare.accept(visitor1);
         node.superExpressSurcharge.accept(visitor2);
-        var fare1 = visitor1.fare;
-        var fare2 = visitor2.fare;
         var lines1 = visitor1.lines;
         var lines2 = visitor2.lines;
 
-        fare = ChildDiscount.computeTotalFareForChild(fare1, fare2);
         lines = sum(
-                List.of(Tuple.of(depth, "oneChild: " + fare.show())),
+                List.of(Tuple.of(depth, "oneChild: " + EvalVisitor.evaluate(node).show())),
                 sum(lines1, lines2)
         );
     }
 
     private static <T> List<T> sum(List<T> l1, List<T> l2) {
-        return Stream.concat(l1.stream(), l2.stream()).collect(Collectors.toUnmodifiableList());
+        return Stream
+                .concat(l1.stream(), l2.stream())
+                .collect(Collectors.toUnmodifiableList());
     }
 }
