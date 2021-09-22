0de6b06971755725aa03fa1d345af3f40a5fa6e0
Kato Naruaki <kato_naruaki@interprism.co.jp>
2021-09-22 15:35:11 +0900

    fix(StationRepositoryInMemorySpec): update test cases

--- src/test/groovy/domain/jr/externalsystems/station/StationRepositoryInMemorySpec.groovy
+++ src/test/groovy/domain/jr/externalsystems/station/StationRepositoryInMemorySpec.groovy
@@ -8,61 +8,38 @@ class StationRepositoryInMemorySpec extends Specification {
     @Shared
     StationRepository repository = new StationRepositoryInMemory()
 
-    def "id = #id の駅は #stationResult"() {
+    def "id = #id の駅は #name"() {
         given:
         Optional<Station> stationOpt = repository.get(new StationId(id))
 
         expect:
-        if (stationOpt.isEmpty()) {
-            assert name == null
-        } else {
-            def station = stationOpt.get()
-            assert station.getName().getName() == name
-            println station
-        }
+        assert stationOpt.isPresent()
+        def station = stationOpt.get()
+        assert station.getName().getName() == name
 
         where:
         id || name
-        -2 || null
-        -1 || null
-        0 || null
-        1 || null
-        2 || null
-        3 || null
-        4 || null
-        5 || null
-        6 || null
-        7 || null
-        8 || null
-        9 || null
         10 || "東京"
-        11 || null
-        12 || null
-        13 || null
-        14 || null
-        15 || null
-        16 || null
-        17 || null
-        18 || null
-        19 || null
         20 || "新大阪"
-        21 || null
-        22 || null
-        23 || null
-        24 || null
-        25 || null
-        26 || null
-        27 || null
-        28 || null
-        29 || null
-        30 || null
-        31 || null
-        32 || null
-        33 || null
-        34 || null
         35 || "姫路"
-        36 || null
+    }
 
-        stationResult = (name == null) ? "未定義" : "< $name >"
+    def "id = #id の駅は存在しない"() {
+        given:
+        Optional<Station> stationOpt = repository.get(new StationId(id))
+
+        expect:
+        assert stationOpt.isEmpty()
+
+        where:
+        id | _
+        0  | _
+        1  | _
+        9  | _
+        11 | _
+        19 | _
+        21 | _
+        34 | _
+        36 | _
     }
 }
